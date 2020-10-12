9f8a5efed6a45519325b8591cec4c966281df250
Mayowa Falade <falademayowa240@gmail.com>
2020-10-12 21:21:49 +0100

    [refactor]: remove unused permissions

--- src/background/index.js
+++ src/background/index.js
@@ -1,20 +1,5 @@
 const GITHUB_URL = 'https://github.com';
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { urlMatches: GITHUB_URL },
-          }),
-        ],
-        actions: [new chrome.declarativeContent.ShowPageAction()],
-      },
-    ]);
-  });
-});
-
 chrome.webNavigation.onHistoryStateUpdated.addListener(({ url }) => {
   if (url.includes(GITHUB_URL)) {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
